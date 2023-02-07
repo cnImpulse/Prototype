98d40a60b50876fffd613d61c25010ab6af503ad
impulse <2535189010@qq.com>
2023-02-08 00:58:33 +0800

    【工具】导表工具命令行

---
+++ 导表工具/run.bat
@@ -0,0 +1,9 @@
+dotnet Luban.ClientServer/Luban.ClientServer.dll -j cfg --^
+    -d Defines/__root__.xml ^
+    --input_data_dir Excels ^
+    --output_code_dir ..\Prototype\Assets\Game\Scripts\Cfg\Gen ^
+    --output_data_dir ..\Prototype\Assets\Game\Res\Cfg\Bytes ^
+    --gen_types code_cs_unity_bin,data_bin ^
+    -s all 
+
+pause
